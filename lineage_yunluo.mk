7bc1d1e08cb33759d7d14126f0c036830ef1b7b1
Glautinor <glautinor@gmail.com>
2025-07-11 17:46:36 +0000

    lineag_yunluo : Key BUILD_FINGERPRINT isn't a valid prop override

--- lineage_yunluo.mk
+++ lineage_yunluo.mk
@@ -14,7 +14,7 @@ $(call inherit-product, device/xiaomi/yunluo/device.mk)
 # Inherit some common lineage Stuff
 $(call inherit-product, vendor/lineage/config/common_full_tablet_wifionly.mk)
 
-PRODUCT_NAME := infinity_yunluo
+PRODUCT_NAME := lineage_yunluo
 PRODUCT_DEVICE := yunluo
 PRODUCT_MANUFACTURER := Xiaomi
 PRODUCT_BRAND := Xiaomi
@@ -23,8 +23,7 @@ PRODUCT_MODEL := Redmi Pad
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    BuildDesc="yunluo-user 12 SP1A.210812.016 V816.0.4.0.ULYMIXM release-keys"
-
-BUILD_FINGERPRINT := Redmi/yunluo_global/yunluo:12/SP1A.210812.016/V816.0.4.0.ULYMIXM:user/release-keys
+    BuildDesc="yunluo-user 12 SP1A.210812.016 V816.0.4.0.ULYMIXM release-keys" \
+    BuildFingerprint=Redmi/yunluo_global/yunluo:12/SP1A.210812.016/V816.0.4.0.ULYMIXM:user/release-keys
 
 #END
